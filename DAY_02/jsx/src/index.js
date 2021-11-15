60f1c14c1cc92717eba1aeccc122a8a49895d78c
arpit gupta <mycontact.arpit@gmail.com>
2021-11-15 19:23:58 +0530

    function created

---
+++ DAY_02/jsx/src/index.js
@@ -0,0 +1,26 @@
+// import react and react dom libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function getButtonText() {
+  return "Click on me!";
+}
+
+// create a react component 
+const App = () => {
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {getButtonText()}
+      </button>
+    </div>
+  );
+};
+
+// Take the react component and show on screen
+ReactDOM.render(<App />, document.querySelector("#root"));
