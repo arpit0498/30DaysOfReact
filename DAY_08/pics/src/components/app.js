f1d1a39b9e08310b07ffff3c5bba66a645b0f4cd
arpit gupta <mycontact.arpit@gmail.com>
2021-11-23 15:34:26 +0530

    refactor: asyn wait for the axios response

--- DAY_08/pics/src/components/app.js
+++ DAY_08/pics/src/components/app.js
@@ -3,18 +3,18 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term){
-        axios.get('https://api.unsplash.com/search/photos',{
+    async onSearchSubmit(term){
+        const response=await axios
+        .get('https://api.unsplash.com/search/photos',{
             params:{query: term},
             headers:{
                 Authorization: 
                 'Client-ID fmr8sLrruIBiEoWo9pGPI2VvjItNtZUCWFN8J0qRa7k'
             }
 
-        })
-        .then(response => {
-            console.log(response.data.results);
         });
+        console.log(response.data.results);
+        
     }
 
     render(){
