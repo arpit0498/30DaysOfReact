849322e9d3080fb841c7c67d7e921ccad23ceedb
arpit gupta <mycontact.arpit@gmail.com>
2021-12-03 00:34:19 +0530

    refactor

--- DAY_13/widgets/src/components/dropdown.js
+++ DAY_13/widgets/src/components/dropdown.js
@@ -49,7 +49,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     return (
         <div ref={ref} className="ui form">
             <div className="field">
-                <label className="label">Select a Color</label>
+                <label className="label">Select a language</label>
                 <div
                     onClick={() => setOpen(!open)}
                     className={`ui selection dropdown ${open ? "visible active" : ""}`}
