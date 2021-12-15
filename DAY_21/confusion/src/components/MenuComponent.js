837b723d7f998e0d3b0e82b6d8212a87ee16dcb0
arpit gupta <mycontact.arpit@gmail.com>
2021-12-15 17:18:26 +0530

    refactor

--- DAY_21/confusion/src/components/MenuComponent.js
+++ DAY_21/confusion/src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 import DishDetail from './DishdetailComponent';
 
 class Menu extends Component {
@@ -7,12 +7,12 @@ class Menu extends Component {
     constructor(props) {
         super(props);
 
-        // stores iproperties of this component
+
         this.state = {
             selectedDish: null
         };
 
-        console.log('Menu component constructed');
+
 
     }
 
@@ -24,10 +24,30 @@ class Menu extends Component {
 
     }
 
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle> {dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+
+                    </CardBody>
+
+                </Card>
+            );
+
+        } else {
+            return (
+                <div></div>
+            );
+        }
+    }
+
 
 
     render() {
-        console.log('renders menu component');
 
         const menu = this.props.dishes.map((dish) => {
             return (
@@ -47,6 +67,10 @@ class Menu extends Component {
             <div className="container">
                 <div className="row">
                     {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
+
                 </div>
 
                 <DishDetail dish={this.state.selectedDish} />
